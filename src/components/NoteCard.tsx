15d52a0d377941c16fff64eb5be3623ac024ca26
Oscar Mumm <oscarmumm@gmail.com>
2025-06-02 14:14:06 -0300

    content length on car component reviwed

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,26 +1,35 @@
-import type { Note } from '../types';
-import { useNavigate } from 'react-router';
-import { TiPin } from 'react-icons/ti';
+import type {Note} from '../types'
+import {useNavigate} from 'react-router'
+import {TiPin} from 'react-icons/ti'
 
 type NoteCardProps = {
-    note: Note;
-};
+    note: Note
+}
+// REVISAR CORTE DEL TITLE
 
-export default function NoteCard({ note }: NoteCardProps) {
-    const navigate = useNavigate();
+export default function NoteCard({note}: NoteCardProps) {
+    const navigate = useNavigate()
     const handleClick = () => {
-        navigate(`note/${note.id}`);
-    };
+        navigate(`note/${note.id}`)
+    }
     return (
         <div
             className={`${note.color || 'bg-sky-900'} p-3 rounded-lg shadow-xl`}
-            onClick={handleClick}
-        >
-            <div className='flex justify-between mb-3'>
-                <h3 className='font-bold text-lg'>{note.title}</h3>
-                    {note.pinned && <TiPin className='min-w-6 h-6' />}
+            onClick={handleClick}>
+            <div className="flex justify-between mb-3">
+                <h3 className="font-bold text-lg">{note.title}</h3>
+                {note.title.length > 30 ? (
+                    <p>{note.title.slice(0, 30)}...</p>
+                ) : (
+                    <p>{note.title}</p>
+                )}
+                {note.pinned && <TiPin className="min-w-6 h-6" />}
             </div>
-            <p>{note.content}</p>
+            {note.content.length > 50 ? (
+                <p>{note.content.slice(0, 50)}...</p>
+            ) : (
+                <p>{note.content}</p>
+            )}
         </div>
-    );
+    )
 }
--- src/components/NotesGrid.tsx
+++ src/components/NotesGrid.tsx
@@ -1,31 +1,35 @@
-import type { Note } from '../types';
-import NoteCard from './NoteCard';
+import type {Note} from '../types'
+import NoteCard from './NoteCard'
 
 type NotesGridProps = {
-    notes: Note[];
-};
+    notes: Note[]
+}
 
-export default function NotesGrid({ notes }: NotesGridProps) {
-    const pinnedNotes = notes.filter((note) => note.pinned);
-    const notPinnedNotes = notes.filter((note) => !note.pinned);
+export default function NotesGrid({notes}: NotesGridProps) {
+    const pinnedNotes = notes.filter((note) => note.pinned)
+    const notPinnedNotes = notes.filter((note) => !note.pinned)
     return (
         <div>
-            <div className='p-3'>
-                <h3 className='p-3'>Pinned Notes</h3>
-                <div className='grid grid-cols-2 gap-3'>
+            <div className="p-3">
+                {pinnedNotes.length >= 1 && (
+                    <h3 className="p-3">Pinned Notes</h3>
+                )}
+                <div className="grid grid-cols-2 gap-3">
                     {pinnedNotes.map((note) => (
                         <NoteCard key={note.id} note={note} />
                     ))}
                 </div>
             </div>
-            <div className='p-3'>
-                <h3 className='p-3'>Other</h3>
-                <div className='grid grid-cols-2 gap-3'>
+            <div className="p-3">
+                    {pinnedNotes.length >= 1 && (
+                        <h3 className="p-3">Other</h3>
+                    )}
+                <div className="grid grid-cols-2 gap-3">
                     {notPinnedNotes.map((note) => (
                         <NoteCard key={note.id} note={note} />
                     ))}
                 </div>
             </div>
         </div>
-    );
+    )
 }
